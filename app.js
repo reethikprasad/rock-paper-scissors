f66109c5624dbcb5d5105387b3e58a2dbb47996e
Reethik Prasad <reethikprasad@gmail.com>
2020-06-12 16:38:30 +0530

    Done

--- app.js
+++ app.js
@@ -17,7 +17,14 @@ const game=()=>{
     const playMatch = ()=> {
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
-        const computerHand = document.querySelectorAll(".computer-hand");
+        const computerHand = document.querySelector(".computer-hand");
+        const hands = document.querySelectorAll(".hands img");
+
+        hands.forEach(hand => {
+            hand.addEventListener("animationend", function() {
+              this.style.animation = "";
+            });
+          });
         //comp options
         const computerOptions=["rock","paper","scissors"];
 
@@ -26,30 +33,83 @@ const game=()=>{
 
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice=computerOptions[computerNumber];
-            
-            });
+            //here we call fns
+            setTimeout(() => {
+                //Here is where we call compare hands
+                compareHands(this.textContent, computerChoice);
+                //Update Images
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
+              }, 2000);
+              //Animation
+                  //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+        
+        
+        });
         });
         
      };
 
-     const computerHands=(playerChoice,computerChoice)=>{
+     const updateScore = () => {
+        const playerScore = document.querySelector(".player-score p");
+        const computerScore = document.querySelector(".computer-score p");
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+      };
 
-        const winner=document.queryCommandEnabled('.winner');
-
-        if(playerChoice===computerChoice){
-            winner.textContent='It is a tie';
-            return;
+     const compareHands = (playerChoice, computerChoice) => {
+        //Update Text
+        const winner = document.querySelector(".winner");
+        //Checking for a tie
+        if (playerChoice === computerChoice) {
+          winner.textContent = "It is a tie";
+          return;
         }
-
-        if(playerChoice==='rock'){
-            winner.textContent='Player Wins'
-        }else{
-            winner.textContent='Computer Wins'
-            return;
+         //Check for Rock
+    if (playerChoice === "rock") {
+        if (computerChoice === "scissors") {
+          winner.textContent = "Player Wins";
+          pScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore();
+          return;
         }
-
-     };
-
+      }
+      //Check for Paper
+      if (playerChoice === "paper") {
+        if (computerChoice === "scissors") {
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Player Wins";
+          pScore++;
+          updateScore();
+          return;
+        }
+      }
+      //Check for Scissors
+      if (playerChoice === "scissors") {
+        if (computerChoice === "rock") {
+          winner.textContent = "Computer Wins";
+          cScore++;
+          updateScore();
+          return;
+        } else {
+          winner.textContent = "Player Wins";
+          pScore++;
+          updateScore();
+          return;
+        }
+      }
+    }; 
     startGame();
     playMatch();
 };
